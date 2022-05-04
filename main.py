39848dea09fcd5c12da255af8e90081c70115923
Bryan Finkel <bryan2018@Bryans-MacBook-Pro.local>
2022-05-04 06:09:03 -0700

    HW API 6 trying to use pydantic model to receive variables from postman

--- main.py
+++ main.py
@@ -63,10 +63,25 @@ def add (X,Y):
 
 #API 6: POST /api/v1/math/{operation} -> execute +,-,*,/ or return 404 if operation invalid
 # First POST, not a GET
-# API 5: GET /api/v1/math/{operation}/{variable1}/(variable2} -> execute +,-,*,/ or return 404 if operation invalid
 # /api/v1/math/add/1/2
+
+# following class taken from pydantic
+class Item(BaseModel):
+    variable1: int
+    variable2: int
+    operation: str | None = None
+'''
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+@app.post("/api/v1/math/{operation}/items/")
+async def PerformOperation6(operation: str, item: Item):
+async def PerformOperation6(item: Item):
+'''
 @app.post("/api/v1/math/{operation}")
-async def PerformOperation6(operation: str,variable1: int,variable2: int):
+async def PerformOperation6(operation: str, item: Item):
+    #variable1: int
+    #variable2: int
     if operation == "add":
         ops=variable1+variable2
         return {"ops": ops}
